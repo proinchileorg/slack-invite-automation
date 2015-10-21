58c1631c0ca1bd676c02c425932ea9d29f7828de
diruzcode <infojobrc@rayalab.cl>
2015-10-21 14:18:10 -0300

    Upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // your community or team name to display on join page.
+  community: process.env.COMMUNITY_NAME || 'PROGRAMADORES E INFORMATICOS CHILE',
+  // your slack team url (ex: socketio.slack.com)
+  slackUrl: process.env.SLACK_URL || 'programadoresch.slack.com',
+  // access token of slack
+  // You can generate it in https://api.slack.com/web#auth
+  // You should generate the token in admin user, not owner.
+  // If you generate the token in owner user, missing_scope error will be occurred.
+  //
+  // You can test your token via curl:
+  //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
+  //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
+  //   --compressed
+  slacktoken: process.env.SLACK_TOKEN || 'TOKEN AUTENTIFICATION'
+};
